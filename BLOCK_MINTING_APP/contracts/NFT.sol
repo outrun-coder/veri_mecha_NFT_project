73f16ee1b81cba3d13d44aad595887f661248741
David Pineda <david.pineda.orvc@gmail.com>
2023-04-23 16:31:46 -0400

    Setup/planning features (#3)
    * Add internal KANBAN board for progress tracking
    
    * Research minutes for NFT MASTERCLASS - 11/2022. | DAPPU
    
    * start script for hardhat node
    
    * Stub out and plan for NFT contract API and feature set planning
    
    * Stub out NFT testing & high level goals
    
    * Update project plan

---
+++ BLOCK_MINTING_APP/contracts/NFT.sol
@@ -0,0 +1,39 @@
+// START
+// TODO [] - initialize
+// TODO [] - smoke test
+
+// DEPLOYMENT & INIT
+// TODO [] - start deployment args
+// TODO [] - contract scope state
+
+// TODO [] - review and import Open Zepplin libs
+
+// BASE FEATURES
+// TODO [] - MINTING
+// TODO [] - getTokenGroupsOwnedBy(address _user)
+// TODO [] - getTokenURI
+// TODO [] - getTokenMeta
+// TODO [] - getTokenImage
+
+// MANAGEMENT - PUBLIC
+// TODO [] - getTimeUntilMinting
+// TODO [] - isGroupRevealed
+
+// MANAGEMENT - BY_OWNER
+// GROUP MINT
+// TODO [] - setGroupMintDate
+// TODO [] - setGroupMintStatus [PENDING, OPEN, PAUSED, CLOSED]
+// TODO [] - setGroupMintingCostTo
+// TODO [] - setGroupMaxMintAmount
+// GROUP META
+// TODO [] - setGroupAsRevealed
+// TODO [] - setGroupStagedURI
+// TODO [] - setGroupTokenPendingURI
+// TODO [] - setGroupBaseURI
+// GENERAL
+// TODO [] - setBaseExtension
+// TODO [] - witdrawFundsRaised
+// TODO [] - ___
+
+// EXTRA
+// TODO [] - ADDITIVE / AMENDMENT COLLECTIONS
---
+++ BLOCK_MINTING_APP/test/NFT.ts
@@ -0,0 +1,40 @@
+// Stub out basic functionality requiremetns
+
+
+// TODO [] - LIST / IMPORT
+// - DEPS
+// - UTILS
+
+// CONTRACT
+// TODO [] - DESCRIBE CONTRACT & PROPS
+
+// TODO [] - DETERMINE ACTORS
+
+
+// TODO [] -
+// ! PROCESS MANAGEMENT
+
+// ! - PROCESS DEPLOYMENT
+// ! - PROCESS GROUP MIGRATION
+
+// ! - UPDATE GROUP MINTING
+// ! - UPDATE GROUP META
+  
+// ! - UPDATE CONTRACT META
+// ! - PROCESS WITDRAWL OF FUNDS RAISED
+
+
+// TODO [] -
+// ! ACCESS CONTRACT
+// - DETAILS
+// - STATE
+// - RECORDS
+// - TOKEN DETAILS
+
+
+// TODO [] -
+// ! MINTING
+
+
+//
+// TODO [] -
