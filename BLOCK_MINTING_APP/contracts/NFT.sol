010d4860b487e916599a8e73c54f3b234c70d2f7
David Pineda <david.pineda.orvc@gmail.com>
2023-04-23 12:19:13 -0400

    Stub out and plan for NFT contract API and feature set planning

---
+++ BLOCK_MINTING_APP/contracts/NFT.sol
@@ -0,0 +1,35 @@
+// START
+// TODO [] - initialize
+// TODO [] - smoke test
+// TODO [] - start deployment args
+// TODO [] - review and import Open Zepplin libs
+// TODO [] - contract scope state
+
+// BASE FEATURES
+// TODO [] - MINTING
+// TODO [] - getTokenGroupsOwnedBy(address _user)
+// TODO [] - getTokenURI
+// TODO [] - getTokenMeta
+// TODO [] - getTokenImage
+
+// MANAGEMENT - PUBLIC
+// TODO [] - getTimeUntilMinting
+// TODO [] - isGroupRevealed
+
+// MANAGEMENT - BY_OWNER
+// GROUP
+// TODO [] - setGroupMintDate
+// TODO [] - setGroupMintStatus [PENDING, OPEN, PAUSED, CLOSED]
+// TODO [] - setGroupAsRevealed
+// TODO [] - setGroupMintingCostTo
+// TODO [] - setGroupMaxMintAmount
+// TODO [] - setGroupStagedURI
+// TODO [] - setGroupTokenPendingURI
+// TODO [] - setGroupBaseURI
+// GENERAL
+// TODO [] - setBaseExtension
+// TODO [] - witdrawFundsRaised
+// TODO [] - ___
+
+// EXTRA
+// TODO [] - ADDITIVE / AMENDMENT COLLECTIONS
