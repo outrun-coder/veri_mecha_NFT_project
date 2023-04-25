236a3534d48d7b615e70c6786b48c17d5a8b3ef2
David Pineda <david.pineda.orvc@gmail.com>
2023-04-25 00:21:39 -0400

    Block/initialize nft contract (#5)
    * planning updates
    
    * update planning
    
    * Initialize NFT_POC contract and smoke test
    
    * Update deployment sig and test reqs + formatting

--- BLOCK_MINTING_APP/contracts/NFT.sol
+++ BLOCK_MINTING_APP/contracts/NFT.sol
@@ -1,39 +1,66 @@
-// START
-// TODO [] - initialize
-// TODO [] - smoke test
-
-// DEPLOYMENT & INIT
-// TODO [] - start deployment args
-// TODO [] - contract scope state
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
 // TODO [] - review and import Open Zepplin libs
 
-// BASE FEATURES
-// TODO [] - MINTING
-// TODO [] - getTokenGroupsOwnedBy(address _user)
-// TODO [] - getTokenURI
-// TODO [] - getTokenMeta
-// TODO [] - getTokenImage
+// OK [x] - start deployment args
+struct DeploymentArgs {
+  string _name;
+  string _symbol;
+  uint256 _baseCost;
+  // uint256 _maxSupply; // N/A
+  // string _baseTokenStagedURI;
+  // string _baseTokenPendingURI;
+  string _baseTokenURI;
+}
 
-// MANAGEMENT - PUBLIC
-// TODO [] - getTimeUntilMinting
-// TODO [] - isGroupRevealed
+contract NFT_POC {
+  // DEPLOYMENT & INIT
+  // OK [x] - contract scope state
+  string public xName;
+  string public xSymbol;
+  uint256 public xBaseCost;
+  string public xBaseURI;
 
-// MANAGEMENT - BY_OWNER
-// GROUP MINT
-// TODO [] - setGroupMintDate
-// TODO [] - setGroupMintStatus [PENDING, OPEN, PAUSED, CLOSED]
-// TODO [] - setGroupMintingCostTo
-// TODO [] - setGroupMaxMintAmount
-// GROUP META
-// TODO [] - setGroupAsRevealed
-// TODO [] - setGroupStagedURI
-// TODO [] - setGroupTokenPendingURI
-// TODO [] - setGroupBaseURI
-// GENERAL
-// TODO [] - setBaseExtension
-// TODO [] - witdrawFundsRaised
-// TODO [] - ___
+  // OK [x] - initialize
+  // OK [x] - smoke test
+  constructor(DeploymentArgs memory args) {
+    xName = args._name;
+    xSymbol = args._symbol;
+    xBaseCost = args._baseCost;
+    //
+    xBaseURI = args._baseTokenURI;
+  }
 
-// EXTRA
-// TODO [] - ADDITIVE / AMENDMENT COLLECTIONS
+  // BASE FEATURES
+  // TODO [] - MINTING
+  // TODO [] - getTokenGroupsOwnedBy(address _user)
+  // TODO [] - getTokenURI
+  // TODO [] - getTokenMeta
+  // TODO [] - getTokenImage
+
+  // MANAGEMENT - PUBLIC
+  // TODO [] - getTimeUntilMinting
+  // TODO [] - isGroupRevealed
+
+  // MANAGEMENT - BY_OWNER
+  // GROUP MINT
+  // TODO [] - setGroupMintDate
+  // TODO [] - setGroupMintStatus [PENDING, OPEN, PAUSED, CLOSED]
+  // TODO [] - setGroupMintingCost
+  // TODO [] - setGroupMaxMintAmount
+  // GROUP META
+  // TODO [] - setGroupAsRevealed
+  // TODO [] - setGroupStagedURI
+  // TODO [] - setGroupPendingURI
+  // TODO [] - setGroupBaseURI
+  // GENERAL
+  // TODO [] - setBaseExtension
+  // TODO [] - witdrawFundsRaised
+  // TODO [] - ___
+
+  // EXTRA
+  // TODO [] - ADDITIVE / AMENDMENT COLLECTIONS
+  // TODO [] - CONSIDER PROXY UPGRADEABLE MODEL
+
+}
--- BLOCK_MINTING_APP/test/NFT.ts
+++ BLOCK_MINTING_APP/test/NFT.ts
@@ -1,43 +1,94 @@
+// OK [x] - LIST / IMPORT
+// - DEPS
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { Contract } from 'ethers';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+
+// - UTILS
+// TODO [] - create a renderResult util
 import { testerUtil } from "block-project-utils";
-
 const apple = testerUtil
-
 console.log(`>> CONFIRM TESTER:`, apple);
 
-// TODO [] - LIST / IMPORT
-// - DEPS
-// - UTILS
 
-// CONTRACT
-// TODO [] - DESCRIBE CONTRACT & PROPS
+// CONTRACT TEST SUITE
+describe('NFT_POC', () => {
+// OK [x] - DESCRIBE CONTRACT & PROPS
 
-// TODO [] - DETERMINE ACTORS
+  const CONTRACT_PROPS = {
+    _name: 'Veri-Mecha',
+    _symbol: 'VMECH',
+    _baseCost: 1, // fix - .01 throws underflow error
+    //
+    // _baseTokenStagedURI: '',
+    // _baseTokenPendingURI: '',
+    _baseTokenURI: 'asdf',
+  }
 
+  // SUITE CONSTANTS
+  let nftContract: Contract;
+  // trx: ?
+  let deployer: SignerWithAddress;
 
-// TODO [] -
-// ! PROCESS MANAGEMENT
-
-// ! - PROCESS DEPLOYMENT
-// ! - PROCESS GROUP MIGRATION
-
-// ! - UPDATE GROUP MINTING
-// ! - UPDATE GROUP META
-  
-// ! - UPDATE CONTRACT META
-// ! - PROCESS WITDRAWL OF FUNDS RAISED
-
-
-// TODO [] -
-// ! ACCESS CONTRACT
-// - DETAILS
-// - STATE
-// - RECORDS
-// - TOKEN DETAILS
+  // TODO [] - DETERMINE ACTORS
+  beforeEach(async () => {
+    const accounts = await ethers.getSigners();
+    [
+      deployer
+    ] = accounts;
+  });
 
 
-// TODO [] -
-// ! MINTING
+  // OK [x] - DEPLOYMENT
+  describe('Deployment of', () => {
 
+    beforeEach(async () => {
+      const NFT_factory = await ethers.getContractFactory('NFT_POC');
+      // OK [x] - Consolidate deployment signature to a single interface
+      nftContract = await NFT_factory.deploy(CONTRACT_PROPS);
+    });
+
+    it('has correct name', async () => {
+      expect(await nftContract.xName()).to.equal(CONTRACT_PROPS._name);
+    });
+    it('has correct symbol', async () => {
+      expect(await nftContract.xSymbol()).to.equal(CONTRACT_PROPS._symbol);
+    });
+    it('has correct base cost', async () => {
+      expect(await nftContract.xBaseCost()).to.equal(CONTRACT_PROPS._baseCost);
+    });
+    // TODO - RETURNS A COLLECTION GROUP(s)
+    it('has correct base URI', async () => {
+      expect(await nftContract.xBaseURI()).to.equal(CONTRACT_PROPS._baseTokenURI);
+    });
+    // it('returns the owner', async () => {
+    //   expect(await nftContract.owner()).to.equal(deployer.address); // FIX - needs OZ API 
+    // });
+  });
+
+  // ! PROCESS MANAGEMENT
+
+  // ! - PROCESS GROUP MIGRATION
+  // ! - UPDATE GROUP MINTING
+  // ! - UPDATE GROUP META
+    
+  // ! - UPDATE CONTRACT META
+  // ! - PROCESS WITDRAWL OF FUNDS RAISED
+
+
+  // TODO [] -
+  // ! ACCESS CONTRACT
+  // - DETAILS
+  // - STATE
+  // - RECORDS
+  // - TOKEN DETAILS
+
+
+  // TODO [] -
+  // ! MINTING
+
+});
 
 //
 // TODO [] -
