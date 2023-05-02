76d5e0452cde84eccf661757a41f3b6a6d38f6df
David Pineda <david.pineda.orvc@gmail.com>
2023-05-02 07:48:27 -0400

    Start NftMinting

--- UI_MINTING_APP/src/routes/mint/+page.svelte
+++ UI_MINTING_APP/src/routes/mint/+page.svelte
@@ -1,8 +1,8 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import NftMinting from "../../components/interface/nft-minting.svelte";
+
+</script>
 
 <h1>Mint</h1>
 
-<h2>Form</h2>
-
-<h2>Success</h2>
-<h2>Failure</h2>
+<NftMinting/>
