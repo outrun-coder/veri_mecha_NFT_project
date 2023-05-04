e55b35c84af1b2ff063d52f247827d19f3291559
David Pineda <david.pineda.orvc@gmail.com>
2023-05-04 01:29:10 -0400

    initializing provider and nftContract with NftMintingApp.setupNetworkConnections

--- UI_MINTING_APP/src/services/nft-minting.app.ts
+++ UI_MINTING_APP/src/services/nft-minting.app.ts
@@ -1,20 +1,50 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { browser } from '$app/environment'
+
+import { ethers } from 'ethers'
+
 import { networkConfigs } from "../spec-config"
+import NFT_VM_ABI from '../abis/NFT_VM.json';
 
 class NftMintingApp {
 
   store: any
-  contract: any
+  
+  provider: any
+  network: any
+  chainId = "0000";
+
+  nftContract: any
   user: any
 
+  setupNetworkConnections = async() => {
+    //@ts-ignore
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const network = await provider.getNetwork();
+    const chainId = network.chainId.toString();
+
+    const { nft_VM } = networkConfigs[chainId];
+    const nftContract = new ethers.Contract(nft_VM.address, NFT_VM_ABI, provider);
+
+    this.provider = provider;
+    this.network = network;
+    this.chainId = chainId;
+    //
+    this.nftContract = nftContract;
+  }
+
   constructor() {
-    // initialization here <<<
     // TODO [] - STORE FOR RECORD MANAGEMENT
-    // TODO [] - ETHERS CONNECTION
     // [] - USER
-    // [] - CONTRACT
+
     this.store = "TEST_STORE"
-    this.contract = networkConfigs[31337].nft_VM.address;
+
+    if (browser) {
+      this.setupNetworkConnections();
+    }
+    
     this.user = "TEST_USER"
+
   }
 }
 
--- UI_MINTING_APP/src/spec-config/index.ts
+++ UI_MINTING_APP/src/spec-config/index.ts
@@ -1,5 +1,5 @@
 
-export const networkConfigs = {
+export const networkConfigs: {[index: string]:any} = {
   "31337": {
     nft_VM: {
       address: "0x5FbDB2315678afecb367f032d93F642f64180aa3"
