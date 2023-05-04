9b1bc9e035ca5d63c7ccc53e37bcca67170a63a0
David Pineda <david.pineda.orvc@gmail.com>
2023-05-03 22:59:15 -0400

    Start and block out the NFTMinting service

---
+++ UI_MINTING_APP/src/services/nft-minting.app.ts
@@ -0,0 +1,22 @@
+
+class NftMintingApp {
+
+  store: any
+  contract: any
+  user: any
+
+  constructor() {
+    // initialization here <<<
+    // TODO [] - STORE FOR RECORD MANAGEMENT
+    // TODO [] - ETHERS CONNECTION
+    // [] - USER
+    // [] - CONTRACT
+    this.store = "TEST_STORE"
+    this.contract = "TEST_CONTRACT"
+    this.user = "TEST_USER"
+  }
+}
+
+const NftMinting = new NftMintingApp();
+
+export default NftMinting;
