ddce265b0793a78d28cdfbf17d72c198a166d093
David Pineda <david.pineda.orvc@gmail.com>
2023-05-05 00:59:32 -0400

    support for absolute path imports

--- UI_MINTING_APP/vite.config.ts
+++ UI_MINTING_APP/vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -8,5 +9,11 @@ export default defineConfig({
 	},
 	ssr: {
 		noExternal: ['@popperjs/core']
+	},
+	resolve: {
+		alias: {
+			'services': path.resolve('./src/services'),
+			'components': path.resolve('./src/components')
+		}
 	}
 });
