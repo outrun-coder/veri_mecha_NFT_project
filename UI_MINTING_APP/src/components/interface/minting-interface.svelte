6d29f0a70359283fcdb1b97e056efc8ae8313105
David Pineda <david.pineda.orvc@gmail.com>
2023-05-06 06:28:27 -0400

    add / abstract the mint/staging sub interface

--- UI_MINTING_APP/src/components/interface/minting-interface.svelte
+++ UI_MINTING_APP/src/components/interface/minting-interface.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   import { Card, CardBody } from "sveltestrap";
+
   import NftMinting from "services/nft-minting.app";
+	
+  import Staging from "./minting-interface/staging.svelte";
 
   console.log(`> SMOKE TEST NFT MINTING:`, NftMinting);
-  
 </script>
 
 <Card>
   <CardBody>
-    <h2>Mint the Mecha!</h2>
+    <Staging/>
   </CardBody>
 </Card>
---
+++ UI_MINTING_APP/src/components/interface/minting-interface/staging.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Button } from "sveltestrap";
+
+  export const addMintSlot = (e: any) => {
+    console.log('>> ADD SLOT WAS INVOKED:', e);
+  };
+  export const mintX = (e: any) => {
+    console.log('>> HANDLE WAS INVOKED:', e);
+  };
+</script>
+
+<Button on:click={addMintSlot}>+</Button>
+  <h2>Mint the Mecha!</h2>
+<Button on:click={mintX}>Mint Mecha</Button>
+
+<style></style>
