3713df9ea1d09b8eb1f236128de55c8884c18a2d
David Pineda <david.pineda.orvc@gmail.com>
2023-05-06 21:42:35 -0400

    Add UI blocking ProcessingScreen component

---
+++ UI_MINTING_APP/src/components/display/processing-screen.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Spinner } from "sveltestrap";
+</script>
+
+
+<div class="processing-screen">
+  <Spinner color="dark" type="grow"/>
+  <span class="process-message">Processing...</span>
+</div>
+
+
+<style>
+  .processing-screen {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    top: 0px;
+    left: 0px;
+    opacity: .65;
+  }
+
+  .process-message {
+    position: relative;
+    left: 7px;
+    margin-top: 20px;
+  }
+</style>
--- UI_MINTING_APP/src/components/interface/minting-interface.svelte
+++ UI_MINTING_APP/src/components/interface/minting-interface.svelte
@@ -5,6 +5,7 @@
   import NftMinting from "services/nft-minting.app";
 	
   import Staging from "./minting-interface/staging.svelte";
+  import ProcessingScreen from "components/display/processing-screen.svelte";
 
   console.log(`> SMOKE TEST NFT MINTING:`, NftMinting);
 
@@ -24,6 +25,10 @@
 <Card>
   <CardBody>
     <Staging
-    handleMintingWith={handleMintingWith}/>
+      handleMintingWith={handleMintingWith}/>
+
+    {#if $appIsWorking}
+      <ProcessingScreen/>
+    {/if}
   </CardBody>
 </Card>
