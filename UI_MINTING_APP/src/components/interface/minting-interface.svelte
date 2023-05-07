7e29bd5df51dc79a3fbc8be0742ab42fdbc78175
David Pineda <david.pineda.orvc@gmail.com>
2023-05-07 09:05:19 -0400

    clean up

--- UI_MINTING_APP/src/components/interface/minting-interface.svelte
+++ UI_MINTING_APP/src/components/interface/minting-interface.svelte
@@ -15,26 +15,7 @@
   // console.log(`> SMOKE TEST NFT MINTING LOADED:`, NftMinting);
 
   // STATE
-  // let displaySuccessfulMints: boolean = true;
-
   const { appIsWorking } = NftMinting;
-
-  // ACTIONS
-  const handleMintingWith = async(requestedCount: number) => {
-    const result = await NftMinting.processMintsBy(requestedCount);
-
-    console.log('>> INTERFACE RECEIVED RESULT: \n', result);
-
-    if (result.success) {
-      // OK & open interface
-      await NftMinting.setTotalMintsLeft();
-      // proceed to success & display stage
-    } else {
-      // FAIL HANDLE AND TRY AGAIN
-      // open
-      return result;
-    }
-  };  
 </script>
 
 <Card>
