80daaf4bd305d8ba814bdfdef96960e3031f5a00
David Pineda <david.pineda.orvc@gmail.com>
2023-05-11 14:28:41 -0400

    Mod hardhat.config for goerli deployment

--- BLOCK_MINTING_APP/hardhat.config.ts
+++ BLOCK_MINTING_APP/hardhat.config.ts
@@ -1,8 +1,19 @@
+require("dotenv").config();
+
+const privateKeys = process.env.PRIVATE_KEYS || "";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    localhost: {},
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(',')
+    }
+  }
 };
 
 export default config;
